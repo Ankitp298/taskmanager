6c4c5f234a346323edae0e6f19826067fc902503
Ankitp298 <168113068+Ankitp298@users.noreply.github.com>
2025-03-14 14:30:25 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+module.exports = {
+  darkMode: 'class', 
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
